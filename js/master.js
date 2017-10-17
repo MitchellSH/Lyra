94d9fe950b75f108d89b46ec5849d35d7353790f
Mitchell Hollander <mitchell.hollander@cybera.net>
2017-10-17 16:59:44 -0500

    playing with json datasets

--- js/master.js
+++ js/master.js
@@ -2,8 +2,27 @@ $(document).ready(initialize);
 
 function initialize(){
   materialize();
+  getSpaceDataForFun();
 }
 
 function materialize(){
   $(".button-collapse").sideNav();
+}
+
+function getSpaceDataForFun(){
+  var src = "http://api.open-notify.org/astros.json";
+  $.ajax({
+    url: src,
+    type: 'GET',
+    dataType: 'json',
+    success: populateDOMWithFunData,
+    error: function() {
+        console.log('Uh Oh!');
+    },
+  });
+}
+
+function populateDOMWithFunData(data){
+  console.log('Success!');
+  console.log(data);
 }
